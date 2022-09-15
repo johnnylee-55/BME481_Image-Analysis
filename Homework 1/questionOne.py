c66a5efa4e4ccbc03b67b142b4adfdb2d408ee95
johnnylee-55 <leejohnny5533@gmail.com>
2022-09-14 17:20:37 -0700

    sync

--- Homework 1/questionOne.py
+++ Homework 1/questionOne.py
@@ -1,13 +1,14 @@
 from PIL import Image,ImageOps
 import matplotlib.pyplot as plt
-import cv2
 import numpy as np
+
+
 
 #open the image
 #mac
-img = Image.open("Winter.png")
+#img = Image.open("Winter.png")
 #windows
-#img = Image.open([enter filepath here])
+img = Image.open("BME 481\Winter.png")
 
 #color
 img.show()
@@ -15,4 +16,7 @@ img.show()
 imgGray = ImageOps.grayscale(img)
 imgGray.show()
 
+
 #plot grayscale histogram
+imgGrayR = np.ravel(imgGray)
+plt.hist(imgGrayR, 256, [0, 255])
