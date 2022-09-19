cd74034a2c121a20ef77386d1cb94a6e8b6cb6dd
johnny lee <johnnylee@turtle-v4.local>
2022-09-18 21:14:36 -0700

    improved question four portion

--- Homework 1/Four.py
+++ Homework 1/Four.py
@@ -6,17 +6,21 @@ import numpy as np
 img = Image.open("nature.JPG")
 img.show()
 
-#enhance image
-#{
-# enhancerB = ImageEnhance.Brightness(img)
-# imgE = enhancerB.enhance(2.0).show()
+# dimensions
+left = 300
+top = 500
+right = 675
+bottom = 875
+# crop to dimensions
+imgCropped = img.crop((left, top, right, bottom))
+imgCropped.show()
 
-# enhancerS = ImageEnhance.Sharpness(img)
-# imgE = enhancerS.enhance(2.0).show()
-# }
+# brightens image
+enhancer = ImageEnhance.Brightness(imgCropped)
+factor = 3 #brightens the image
+imgBrightened = enhancer.enhance(factor)
+imgBrightened.show()
 
-imgE = ImageOps.equalize(img, mask = None).show()
-
-enhancerS = ImageEnhance.Sharpness(imgE)
-imgS = enhancerS.enhance(2.0).show()
-
+# equalizes image
+imgEqualized = ImageOps.equalize(imgBrightened, mask = None)
+imgEqualized.show()
