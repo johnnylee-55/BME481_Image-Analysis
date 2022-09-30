645a1fd53e88c28af40468edc1de2bf86ef85075
johnny lee <johnnylee@turtle-v4.local>
2022-09-30 12:03:59 -0700

    added to problem 2

--- Homework 2/2.py
+++ Homework 2/2.py
@@ -1,7 +1,14 @@
-from PIL import Image
+from PIL import Image, ImageFilter
+import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
 
 # 2) generate a synthetic image
-synthImg = Image.new( mode = "RGB", size = (150, 150), color = (250, 0, 0) )
+synthImg = Image.new( mode = "L", size = (150, 150), color = (50) )
+foreground = Image.new( mode = "L", size = (75, 75), color = (150) )
+
+synthImg.paste(foreground, (75//2, 75//2), mask = foreground)
+
 synthImg.show()
+
+
