81ef7123d248bbe11495413ed3acaaa577b1038c
Johnny Lee <leejohnny5533@gmail.com>
2022-10-25 13:19:43 -0700

    fixed documentation

--- Homework 4/4.1.py
+++ Homework 4/4.1.py
@@ -7,21 +7,22 @@ hwy = cv2.imread('Homework 4/HWY447.png')
 # converts color image to grayscale
 gray = cv2.cvtColor(hwy, cv2.COLOR_BGR2GRAY)
 
-# applies guassian blur to remove background noise
+# applies median blur to remove background noise
 blur = cv2.medianBlur(gray,5)
 
 # canny edge detection: minVal=50, maxVal=150
 edges = cv2.Canny(blur, 50, 150)
 
 # applies probabilistic hough line transform method on 'edges' to obtain line end points
-# returns list of endpoints for each line, with each element in the array as 4 integers (x1, y1, x2, y2)
-lines = cv2.HoughLinesP(edges,
-            1, # Distance resolution in pixels
-            np.pi / 180, # Angle resolution in radians
-            threshold = 100, # Min number of votes for valid line
-            minLineLength = 40, # Min allowed length of line
-            maxLineGap = 20 # Max allowed gap between line for joining them
-            )
+# returns list of endpoints for each line, with each element in the array being 4 integers (x1, y1, x2, y2)
+lines = cv2.HoughLinesP(
+              edges,
+              1, # Distance resolution in pixels
+              np.pi / 180, # Angle resolution in radians
+              threshold = 100, # Min number of votes for valid line
+              minLineLength = 40, # Min allowed length of line
+              maxLineGap = 20 # Max allowed gap between line for joining them
+              )
 
 # iterates over lines, connects endpoints by drawing lines, adds endpoints to new array lines_array
 lines_array = []
